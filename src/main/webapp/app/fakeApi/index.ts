967d9f8eb89505f1286bdc450863c37bfb163f52
louisheimel <louisheimel1@gmail.com>
2019-02-12 23:59:27 -0600

    get a random card

--- src/main/webapp/app/fakeApi/index.ts
+++ src/main/webapp/app/fakeApi/index.ts
@@ -1,3 +1,31 @@
 export default ({
-    add: (a, b) => a + b
+    add: (a, b) => a + b,
+    hitMe: () => {
+        function shuffle(array) {
+            array = [...array];
+            let currentIndex = array.length;
+            let temporaryValue;
+
+            // While there remain elements to shuffle...
+            while (0 !== currentIndex) {
+
+                // Pick a remaining element...
+                const randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+
+                // And swap it with the current element.
+                temporaryValue = array[currentIndex];
+                array[currentIndex] = array[randomIndex];
+                array[randomIndex] = temporaryValue;
+            }
+
+            return array;
+        }
+
+        const suits = ['H', 'D', 'C', 'S'];
+        const cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+        const product = (a, b) => b.reduce((acc, e1) => acc.concat(a.map(e => e1 + e)), []);
+        const randomCard = shuffle(product(cards, suits))[0];
+        return randomCard;
+    }
 });
